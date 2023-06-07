92a0902b975822330f604d8cf3b27ed009cad344
Vaishnavi Patel <vaishnavip41101@gmail.com>
2023-06-07 19:03:29 +0530

    first

---
+++ daythirteen/src/org/tnsif/synchronization/SynchronizationExample.java
@@ -0,0 +1,15 @@
+//driver class
+//program to demonstrate using synchronization
+package org.tnsif.synchronization;
+public class SynchronizationExample {
+
+	public static void main(String[] args) {
+		Power obj=new Power();
+		ThreadOne t1=new ThreadOne(obj);
+		ThreadTwo t2=new ThreadTwo(obj);
+		t1.start();
+		t2.start();
+
+	}
+
+}

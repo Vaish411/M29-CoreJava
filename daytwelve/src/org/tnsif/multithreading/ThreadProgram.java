def3fca299624d10361346f952f84fa6eb40ed33
Vaishnavi Patel <vaishnavip41101@gmail.com>
2023-06-07 19:02:48 +0530

    first

---
+++ daytwelve/src/org/tnsif/multithreading/ThreadProgram.java
@@ -0,0 +1,29 @@
+/*program to demonstrate on creating a thread by
+ * extending a thread class
+ */
+package org.tnsif.multithreading;
+public class ThreadProgram extends Thread{
+	
+	public void run()
+	{
+		System.out.println("Thread is in the running state");
+	}
+	
+	
+
+	public static void main(String[] args) {
+		//ThreadProgram.run();
+		
+		ThreadProgram t=new ThreadProgram();
+		t.start();
+		/*if thraed is in the starting and running state, we can't start 
+		 * again
+		 */
+		//t.start();
+
+		
+		
+
+	}
+
+}
